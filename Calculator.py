a8a646bfb93bfaacc46db12ee44c53bf1de81236
Terozaki <66570891+Terozaki@users.noreply.github.com>
2020-06-07 21:51:40 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -100,7 +100,6 @@ while running:
     print('Version 1.7.8')
     print('This is a calculator')
     print('By Anwen Brookes-Tee\n')
-    print('This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.\n')
     print('Please enter the value of the calculation you wish to perform\n')
     print('1: Basic Calculation')
     print('2: Simultaneous Equations')
